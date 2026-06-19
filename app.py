ba4475f71135075a4d2e03c894a5d663222367e4
Natalia7-cdm <72091008+Natalia7-cdm@users.noreply.github.com>
2026-06-19 14:14:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Extractor de PDF", layout="centered")
 
 # --- EL LOGO SIEMPRE ARRIBA ---
 # Al ponerlo en 900, le quitamos las columnas para que ocupe un buen espacio
-st.image("logo.jpg", width=800) 
+st.image("logo.jpg", width=600) 
 st.write("---") 
 
 # --- 1. SISTEMA DE LOGIN ---
